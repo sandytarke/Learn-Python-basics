83b93e1080e2760884a96f02fe1512d16aa67e47
Sandhya <sandhya.tarke@gmail.com>
2020-04-16 23:15:51 -1100

    Add files via upload
    These are basic programs of Day1 learning

---
+++ math_function.py
@@ -0,0 +1,64 @@
+# Basic mathematics in Python Tutorial
+# Reference - https://www.geeksforgeeks.org/basic-operators-python/
+
+print('\"Arithmetic Operators\"')
+
+a = 15
+b = 4
+
+# Output: a + b = 19
+print('a + b =',a+b)
+
+# Output: a - b = 11
+print('a - b =',a-b)
+
+# Output: a * b = 60
+print('a * b =',a*b)
+
+# Output: a / b = 3.75
+print('a / b =',a/b)
+
+# Output: a // b = 3
+print('a // b =',a//b)
+
+# Output: a ** b = 50625
+print('a ** b =',a**b)
+
+
+print('\n\"Comparison operators\"')
+x = 10
+y = 12
+
+# Output: x > y is False
+print('x > y is',x>y)
+
+# Output: x < y is True
+print('x < y is',x<y)
+
+# Output: x == y is False
+print('x == y is',x==y)
+
+# Output: x != y is True
+print('x != y is',x!=y)
+
+# Output: x >= y is False
+print('x >= y is',x>=y)
+
+# Output: x <= y is True
+print('x <= y is',x<=y)
+
+print('\n\"Logical Operators\"')
+m = True
+n = False
+
+# Output: m and n is False
+print('m and n is',m and n)
+
+# Output: m or n is True
+print('m or n is',m or n)
+
+# Output: not m is False
+print('not m is',not m)
+
+
+
---
+++ print_function.py
@@ -0,0 +1,29 @@
+# Learning Print function
+print('This is \"Sandhya\'s\" programming')
+
+print('This is an example of print funtion')
+
+# Use of Escape character \
+print('We\'re going to learn Python')
+
+# Concatenation works with , 
+print('Learn','Python')
+
+# Print numbers
+print(int(5)+9)
+
+print(float(5.5)+8)
+
+
+# Multi line comment
+'''
+Run this program by command - Python print_function.py
+Output:
+python print_function.py
+This is "Sandhya's" programming
+This is an example of print funtion
+We're going to learn Python
+Learn Python
+14
+13.5
+'''
